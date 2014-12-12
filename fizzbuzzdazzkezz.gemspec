1762615f95440957429b868db572825a388ed2fb
Victor Goff <keeperotphones@gmail.com>
2014-12-11 20:27:58 -0500

    Fixed test, formatting

--- fizzbuzzdazzkezz.gemspec
+++ fizzbuzzdazzkezz.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'fizzbuzzdazzkezz'
-  s.version = '0.0.2'
-  s.summary = "When FizzBuzz is not enough."
-  s.homepage = "http://samuraicoder.net"
-  s.files = "lib/fizzbuzzdazzkezz.rb"
-  s.test_files = ""
-  s.author = "Takaaki Kato"
-  s.email = "takaaki@samuraicoder.net"
+  s.version = '0.0.3'
+  s.summary = 'When FizzBuzz is not enough.'
+  s.description = 'When FizzBuzz is not enough, and you want to make your own rules.'
+  s.homepage = 'https://github.com/kotp/fizzbuzzdazzkezz'
+  s.files = ['README.md', 'lib/fizzbuzzdazzkezz.rb']
+  s.authors = ['Various Authors']
+  s.emails = ['keeperotphones+fizzbuzz@gmail.com']
   s.has_rdoc = true
-  # s.required_ruby_version = '>=1.8.6'
-  # s.required_rubygems_version = 
+  s.required_ruby_version = '>=2.0'
+  s.post_install_message = "Enjoy having more than just your share of FizzBuzz!"
 end
--- lib/fizzbuzzdazzkezz.rb
+++ lib/fizzbuzzdazzkezz.rb
@@ -1,50 +1,48 @@
 class Range
+  # rdoc
+  #   Usage: (1..100).print_fizzbuzzdazzkezz(hash)
+  #
+  #   By default, the following Hash is presented:
+  #       {3 => :Fizz, 5 => :Buzz, 7 => :Dazz , 9 => :Kezz}
+  #   We use symbols as it is cheaper than using strings.
 
-=begin rdoc
-  Usage: (1..100).print_fizzbuzzdazzkezz(hash)
-
-  By default, the following Hash is presented:
-      {3 => :Fizz, 5 => :Buzz, 7 => :Dazz , 9 => :Kezz}
-  We use symbols as it is cheaper than using strings.
-=end
-
-  def print_fizzbuzzdazzkezz(hash = {7 => :Dazz, 5 => :Buzz, 3 => :Fizz , 9 => :Kezz})
-    self.each do |i|
+  def print_fizzbuzzdazzkezz(hash = { 7 => :Dazz, 5 => :Buzz,
+                                      3 => :Fizz, 9 => :Kezz }
+                            )
+    result = []
+    each do |i|
       str = ''
       hash.sort.each do |key, value|
         str += value.to_s if i.modulo(key).zero?
       end
       str = i if str.empty?
-      puts str
+      result << str
     end
+    result * "\n"
   end
 
-  alias print_fbdk print_fizzbuzzdazzkezz
+  alias_method :print_fbdk, :print_fizzbuzzdazzkezz
 end # End of class Range
 
 class Integer
+  # rdoc
+  # Usage: int.fizzbuzz(hash = {3 => :Fizz, 5 => :Buzz})
+  #
+  # Sample:
+  #       results = ''
+  #       (1..100).each do |num|
+  #       results << num.fizzbuzz
+  #       end
+  #
+  #       puts results
 
-=begin rdoc
-Usage: int.fizzbuzz(hash = {3 => :Fizz, 5 => :Buzz})
-
-Sample: 
-      results = ''
-      (1..100).each do |num|
-      results << num.fizzbuzz
-      end
-
-    puts results
-=end
-
-  def fizzbuzz(hash = {3 => :Fizz, 5 => :Buzz})
+  def fizzbuzz(hash = { 3 => :Fizz, 5 => :Buzz })
     results = ''
     hash.to_a.sort.each do |key, value|
       results << value.to_s if modulo(key).zero?
     end
     results.empty? ?  to_s + "\n" : results + "\n"
   end
-  
-  alias fizzbuzzdazzkezz fizzbuzz 
+
+  alias_method :fizzbuzzdazzkezz, :fizzbuzz
 end # end of Class Integer
-
-
