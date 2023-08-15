31cb3602c003e68d398bb7386914c8951194d5e6
KOTP <keeperotphones@gmail.com>
2023-08-15 13:16:12 -0400

    Solve FizzBuzz for Range in terms of Integer
    This is monkey patching, so worth exploring.
    
    Tend to use Refinements instead.
    
    fixes: #3

--- lib/fizzbuzzdazzkezz.rb
+++ lib/fizzbuzzdazzkezz.rb
@@ -1,48 +1,46 @@
 class Range
+
   # rdoc
-  #   Usage: (1..100).print_fizzbuzzdazzkezz(hash)
+  #   Usage: (1..100).print_fizzbuzzdazzkezz(rules)
   #
-  #   By default, the following Hash is presented:
+  #   By default, the following rules are presented:
   #       {3 => :Fizz, 5 => :Buzz, 7 => :Dazz , 9 => :Kezz}
   #   We use symbols as it is cheaper than using strings.
 
-  def print_fizzbuzzdazzkezz(hash = { 7 => :Dazz, 5 => :Buzz,
-                                      3 => :Fizz, 9 => :Kezz }
+  def print_fizzbuzzdazzkezz(rules = { 7 => :Dazz, 5 => :Buzz,
+                                       3 => :Fizz, 9 => :Kezz }
                             )
-    result = []
-    each do |i|
-      str = ''
-      hash.sort.each do |key, value|
-        str += value.to_s if i.modulo(key).zero?
-      end
-      str = i if str.empty?
-      result << str
+    result = ''
+    each do |integer|
+      result << integer.fizzbuzz(rules)
     end
-    result * "\n"
+    result.chomp
   end
 
   alias_method :print_fbdk, :print_fizzbuzzdazzkezz
+
 end # End of class Range
 
 class Integer
+
   # rdoc
-  # Usage: int.fizzbuzz(hash = {3 => :Fizz, 5 => :Buzz})
+  # Usage: integer.fizzbuzz(rules = {3 => :Fizz, 5 => :Buzz})
   #
   # Sample:
   #       results = ''
-  #       (1..100).each do |num|
-  #       results << num.fizzbuzz
+  #       (1..100).each do |number|
+  #       results << number.fizzbuzz
   #       end
   #
   #       puts results
 
-  def fizzbuzz(hash = { 3 => :Fizz, 5 => :Buzz })
-    results = ''
-    hash.to_a.sort.each do |key, value|
-      results << value.to_s if modulo(key).zero?
+  def fizzbuzz(rules = { 3 => :Fizz, 5 => :Buzz })
+    rules.each_with_object(results = '') do |(factor, word)|
+      results << word.to_s if modulo(factor).zero?
     end
-    results.empty? ?  to_s + "\n" : results + "\n"
+    results.empty? ?  to_s << "\n" : results << "\n"
   end
 
   alias_method :fizzbuzzdazzkezz, :fizzbuzz
+
 end # end of Class Integer
